985f3c1542f93762609d707167c10c6d1e4e0072
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-20 17:28:34 +0300

    Mission Passed

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -12,39 +12,41 @@ function SpicyFoodList() {
     console.log(newFood);
   }
 
-  const filteredFoods = filter === "All" ? foods : foods.filter((food) => food.cuisine === filter);
+  const filteredFoods =
+    filter === "All" ? foods : foods.filter((food) => food.cuisine === filter);
 
   const foodList = filteredFoods.map((food) => (
-    <li key={food.id}>  
+    <li key={food.id}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}{" "}
-      <button onClick={() => updateHeatLevel(food.id, food.heatLevel + 1)}> Increase Heat</button>
+      <button onClick={() => updateHeatLevel(food.id, food.heatLevel + 1)}>
+        {" "}
+        Increase Heat
+      </button>
     </li>
   ));
-  
+
   function updateHeatLevel(foodId, newHeatLevel) {
     setFoods((prevFoods) =>
       prevFoods.map((food) =>
-      food.id === foodId ? {...food, heatLevel: newHeatLevel } : food
+        food.id === foodId ? { ...food, heatLevel: newHeatLevel } : food
       )
     );
   }
 
- 
-
-
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
-      <select 
-      value={filter}
-      name="filter"
-      onChange={(e) => setFilter(e.target.value)}>
-  <option value="All">All</option>
-  <option value="American">American</option>
-  <option value="Sichuan">Sichuan</option>
-  <option value="Thai">Thai</option>
-  <option value="Mexican">Mexican</option>
-</select>
+      <select
+        value={filter}
+        name="filter"
+        onChange={(e) => setFilter(e.target.value)}
+      >
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
 
       <ul>{foodList}</ul>
     </div>
