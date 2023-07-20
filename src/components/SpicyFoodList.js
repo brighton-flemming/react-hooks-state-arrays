d61e25ed6e91e554801798c98dcd8ae2a0b798e1
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-20 17:26:31 +0300

    Mission Passed

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { spicyFoods, getNewRandomSpicyFood } from "../data";
+// import { ALL } from "dns";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filter, setFilter] = useState("All");
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
@@ -10,7 +12,9 @@ function SpicyFoodList() {
     console.log(newFood);
   }
 
-  const foodList = foods.map((food) => (
+  const filteredFoods = filter === "All" ? foods : foods.filter((food) => food.cuisine === filter);
+
+  const foodList = filteredFoods.map((food) => (
     <li key={food.id}>  
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}{" "}
       <button onClick={() => updateHeatLevel(food.id, food.heatLevel + 1)}> Increase Heat</button>
@@ -25,10 +29,23 @@ function SpicyFoodList() {
     );
   }
 
+ 
+
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
+      <select 
+      value={filter}
+      name="filter"
+      onChange={(e) => setFilter(e.target.value)}>
+  <option value="All">All</option>
+  <option value="American">American</option>
+  <option value="Sichuan">Sichuan</option>
+  <option value="Thai">Thai</option>
+  <option value="Mexican">Mexican</option>
+</select>
+
       <ul>{foodList}</ul>
     </div>
   );
